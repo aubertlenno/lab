631ca91e3f226b1236cabd53d2aeb14307ec18b3
aubertlenno <lennoaubert@gmail.com>
2022-09-30 17:11:37 +0700

    wow commit!!

---
+++ Session 3.py
@@ -0,0 +1,52 @@
+x = [1, "hello", 2.5]
+# print(x)
+
+# print(x[0])
+
+# x[1] = "world"
+# print(x[1])
+
+# x.append("world")
+# x.insert(1, "world")
+# y = [2, 3, 4]
+# x.extend(y)
+# x.pop()
+# print(len(x))
+# print(x)
+
+# myTuple = (1, "Hello", 2.5)
+# tupleList = list(myTuple)
+# tupleList[1] = "World"
+# myTuple = tuple(tupleList)
+# print(myTuple)
+
+# mySet = {1, "Hello, 2.5"}
+# print(mySet)
+
+thisDict = {
+    "car" : "Honda",
+    "year" : "2000",
+    "city" : "Jakarta"
+}
+# print(thisDict)
+
+# print(thisDict["car"])
+# print(thisDict["year"])
+# print(thisDict["city"])
+
+# thisDict["car"] = "Toyota"
+# thisDict.update({"year": "2010"})
+
+# thisDict["color"] = "yellow"
+
+# thisDict.pop("color")
+
+for i in x:
+    print(i)
+
+index = 0
+while index <= 3:
+    print([index])
+    index += 1
+
+print(thisDict)
